1b98e11c59b0f11b927ab02795ff6f4e8ff06622
Aman Singh Bhogal <45331015+AmanSinghBhogal@users.noreply.github.com>
2023-05-12 13:31:54 +0530

    Frontend Dependencies updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import { About, Header, Skills, Projects, Testimonial, Footer } from './container';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Header />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
