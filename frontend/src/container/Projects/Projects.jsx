5fd032dfe6afd897b39bec76b42f33204cef8685
Aman Singh Bhogal <45331015+AmanSinghBhogal@users.noreply.github.com>
2023-05-15 15:26:11 +0530

    Added on click property to Project cards

--- frontend/src/container/Projects/Projects.jsx
+++ frontend/src/container/Projects/Projects.jsx
@@ -79,47 +79,49 @@ const Projects = () => {
       >
         {
           FilterProjects.map((project, index) => (
-            <div
-              key={index}
-              className='app__projects-item app__flex'
-            >
-              <div className='app__projects-img app__flex'>
-                <img src={urlFor(project.imgURL)} alt={project.name} />
-                <motion.div
-                  whileHover={{opacity: [0,1]}}
-                  transition={{duration: 0.25, ease: 'easeOut' ,staggerChildren: 0.5}}
-                  className='app__projects-hover app__flex'
-                >
-                  <a href={project.projectLink} target='_blank' rel='noreferrer'>
-                    <motion.div 
-                      whileInView={{scale: [0,1]}}
-                      whileHover={{scale: [1,0.9]}}
-                      transition={{duration: 0.25, ease: 'easeOut' ,staggerChildren: 0.5}}
-                      className='app__flex'
-                    >
-                      <AiFillEye />
-                    </motion.div>
-                  </a>
-                  <a href={project.codeLink} target='_blank' rel='noreferrer'>
-                    <motion.div 
-                      whileInView={{scale: [0,1]}}
-                      whileHover={{scale: [1,0.9]}}
-                      transition={{duration: 0.25, ease: 'easeOut' ,staggerChildren: 0.5}}
-                      className='app__flex'
-                    >
-                      <AiFillGithub />
-                    </motion.div>
-                  </a>
-                </motion.div>
-              </div> 
-              <div className='app__projects-content app__flex '>
-                <h4 className='bold-text project-title'>{project.title}</h4>
-                <p className='p-text desc' style={{marginTop: 10}}>{project.description}</p>
-                <div className='app__projects-tag app__flex'>
-                  <p className='p-text'>{`#${project.tags[0]}`}</p>
+            <a href={project.codelink} target='_blank' rel='noreferrer'>
+              <div
+                key={index}
+                className='app__projects-item app__flex'
+              >
+                <div className='app__projects-img app__flex'>
+                  <img src={urlFor(project.imgURL)} alt={project.name} />
+                  <motion.div
+                    whileHover={{opacity: [0,1]}}
+                    transition={{duration: 0.25, ease: 'easeOut' ,staggerChildren: 0.5}}
+                    className='app__projects-hover app__flex'
+                  >
+                    <a href={project.projectLink} target='_blank' rel='noreferrer'>
+                      <motion.div 
+                        whileInView={{scale: [0,1]}}
+                        whileHover={{scale: [1,0.9]}}
+                        transition={{duration: 0.25, ease: 'easeOut' ,staggerChildren: 0.5}}
+                        className='app__flex'
+                      >
+                        <AiFillEye />
+                      </motion.div>
+                    </a>
+                    <a href={project.codelink} target='_blank' rel='noreferrer'>
+                      <motion.div 
+                        whileInView={{scale: [0,1]}}
+                        whileHover={{scale: [1,0.9]}}
+                        transition={{duration: 0.25, ease: 'easeOut' ,staggerChildren: 0.5}}
+                        className='app__flex'
+                      >
+                        <AiFillGithub />
+                      </motion.div>
+                    </a>
+                  </motion.div>
+                </div> 
+                <div className='app__projects-content app__flex '>
+                  <h4 className='bold-text project-title'>{project.title}</h4>
+                  <p className='p-text desc' style={{marginTop: 10}}>{project.description}</p>
+                  <div className='app__projects-tag app__flex'>
+                    <p className='p-text'>{`#${project.tags[0]}`}</p>
+                  </div>
                 </div>
               </div>
-            </div>
+            </a>
           ))
         }
 
