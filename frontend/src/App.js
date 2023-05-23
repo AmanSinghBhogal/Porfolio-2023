bd2aaf8cc7e9f8111cf1fe2f27dc1388bbf5786d
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2023-05-23 18:46:56 +0530

    Added Custom Cursor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,25 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import { About, Header, Skills, Projects, Testimonial, Footer } from './container';
+import AnimatedCursor from "react-animated-cursor";
+
 function App() {
   return (
     <div className="app">
+      <AnimatedCursor
+        innerSize={25}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)'
+        }}
+      />
       <Navbar />
       <Header />
       <About />
